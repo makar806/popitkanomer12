c88bdf35e84f8d18af1bd1ca100d71ee01dc069a
makarhic <lollkasyv@mail.ru>
2020-12-25 19:21:41 +0300

    first commit

---
+++ SpawnObject (2).cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    public int RandoMs;
+    public GameObject Clip;
+    public GameObject[] SpawnPoints;
+
+    void Start()
+    {
+        RandoMs = Random.Range(0, 2);
+        if(RandoMs == 1)
+        {
+            Clip.transform.position = SpawnPoints[0].transform.position;
+        }
+        if (RandoMs == 2)
+        {
+            Clip.transform.position = SpawnPoints[1].transform.position;
+        }
+        if (RandoMs == 3)
+        {
+            Clip.transform.position = SpawnPoints[2].transform.position;
+        }
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
